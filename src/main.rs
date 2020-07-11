e5c09b7175f5ec0a01c05776b41f57f4b85ba72a
harinimaniam <harinimaniam2001@gmail.com>
2020-07-11 14:11:08 +0530

    A Post route for storing uploaded files in tmp directory

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,61 @@
-
 extern crate juniper;
 
 #[macro_use]
 extern crate diesel;
 
 use std::sync::Arc;
+use std::io::Write;
 
-use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_cors::Cors;
+use actix_multipart::Multipart;
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
+use futures::{StreamExt, TryStreamExt};
 
+
+mod commons;
 mod db_manager;
 mod graphql_schema;
 mod models;
 mod schema;
 mod services;
-mod commons;
 
 use db_manager::establish_connection;
 use graphql_schema::{create_gq_schema, DBContext, GQSchema};
+
+#[post("/upload")]
+async fn upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
+
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        
+
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let name = content_type.get_name().unwrap();
+
+        println!("fuzzyId is {:?}",name);
+        
+        let dir_path = format!("./tmp/{}",name);
+        std::fs::create_dir_all(dir_path).unwrap();
+
+        let filepath = format!("./tmp/{}/{}", name,sanitize_filename::sanitize(&filename));
+        
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+        
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    Ok(HttpResponse::Ok().into())
+}
 
 #[get("/graphiql")]
 async fn graphiql() -> HttpResponse {
@@ -31,7 +67,7 @@ async fn graphiql() -> HttpResponse {
 
 #[post("/test")]
 async fn test(request: HttpRequest) -> HttpResponse {
-    println!("{:?}",request);
+    println!("{:?}", request);
     let body = format!("Welcome !!!");
     HttpResponse::Ok().body(body)
 }
@@ -61,8 +97,8 @@ async fn graphql(
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-  
     std::env::set_var("RUST_LOG", "actix_web=info");
+    std::fs::create_dir_all("./tmp").unwrap();
     env_logger::init();
     dotenv::dotenv().ok();
 
@@ -77,18 +113,13 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .data(db_context.clone())
             .data(gq_schema.clone())
-            .wrap(
-                Cors::new() 
-                    .supports_credentials()
-                    .max_age(3600)
-                    .finish(),
-            )
+            .wrap(Cors::new().supports_credentials().max_age(3600).finish())
             .service(test)
             .service(graphql)
             .service(graphiql)
+            .service(upload)
     })
     .bind(&bind)?
     .run()
     .await
 }
-
